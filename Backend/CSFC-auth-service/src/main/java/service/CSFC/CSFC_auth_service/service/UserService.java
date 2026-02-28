0f4ca15a9e0b17ab451080bf5fd98c21f7941872
Nguyễn Võ Hoàng Long <longxautrai12@gmail.com>
2026-02-28 20:36:12 +0700

    [FEATURE]Implement create user with role

--- Backend/CSFC-auth-service/src/main/java/service/CSFC/CSFC_auth_service/service/UserService.java
+++ Backend/CSFC-auth-service/src/main/java/service/CSFC/CSFC_auth_service/service/UserService.java
@@ -1,9 +1,11 @@
 package service.CSFC.CSFC_auth_service.service;
 
+import service.CSFC.CSFC_auth_service.model.dto.request.CreateUserRequest;
 import service.CSFC.CSFC_auth_service.model.dto.response.UserResponse;
+
 
 public interface UserService
 {
     UserResponse getCurrentUser(String email);
-
+    UserResponse CreateUserWithRoleByAdmin(CreateUserRequest request);
 }
