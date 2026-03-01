82ba34446b5d653958c2508d76d103db66f6f835
Nguyễn Võ Hoàng Long <longxautrai12@gmail.com>
2026-03-01 17:10:18 +0700

    [FEATURE]Add deactivate endpoint and restrict delete to inactive users

---
+++ Backend/CSFC-auth-service/src/main/java/service/CSFC/CSFC_auth_service/controller/UserController.java
@@ -0,0 +1,36 @@
+package service.CSFC.CSFC_auth_service.controller;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import service.CSFC.CSFC_auth_service.common.response.BaseResponse;
+import service.CSFC.CSFC_auth_service.model.dto.response.UserResponse;
+import service.CSFC.CSFC_auth_service.service.UserService;
+
+import java.util.UUID;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/users")
+public class UserController {
+    private final UserService userService;
+    //@PreAuthorize("hasAuthority('USER_DELETE')")
+    @DeleteMapping("/{id}")
+    public ResponseEntity<BaseResponse<String>> deleteUser(@PathVariable UUID id) {
+
+        userService.deleteUserByAdmin(id);
+
+        return ResponseEntity.ok(
+                BaseResponse.success("Xóa người dừng thành công", null)
+        );
+    }
+    //@PreAuthorize("hasAuthority('USER_DEACTIVATE')")
+    @PatchMapping("/{id}/deactivate")
+    public ResponseEntity<BaseResponse<String>> deactivateUser(@PathVariable UUID id) {
+        userService.deActivateUserByAdmin(id);
+        return ResponseEntity.ok(BaseResponse.success("Vô hiệu hóa người dùng thành công", null));
+    }
+
+}
--- Backend/CSFC-auth-service/src/main/java/service/CSFC/CSFC_auth_service/service/imp/UserServiceImp.java
+++ Backend/CSFC-auth-service/src/main/java/service/CSFC/CSFC_auth_service/service/imp/UserServiceImp.java
@@ -1,13 +1,19 @@
 package service.CSFC.CSFC_auth_service.service.imp;
 
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import service.CSFC.CSFC_auth_service.common.exception.BadRequestException;
+import service.CSFC.CSFC_auth_service.common.exception.ResourceNotFoundException;
+import service.CSFC.CSFC_auth_service.common.response.BaseResponse;
 import service.CSFC.CSFC_auth_service.mapper.UserMapper;
 import service.CSFC.CSFC_auth_service.model.dto.response.AuthResponse;
 import service.CSFC.CSFC_auth_service.model.dto.response.UserResponse;
 import service.CSFC.CSFC_auth_service.model.entity.Users;
 import service.CSFC.CSFC_auth_service.repository.UsersRepository;
 import service.CSFC.CSFC_auth_service.service.UserService;
+
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -21,5 +27,28 @@ public class UserServiceImp  implements UserService {
                 .orElseThrow(() -> new RuntimeException("Không tìm thấy tài khoản: " + email));
 
         return userMapper.toResponse(users);
+    }
+
+    @Override
+    @Transactional
+    public void deActivateUserByAdmin(UUID userId) {
+
+        Users user = usersRepository.findById(userId)
+                .orElseThrow(() -> new ResourceNotFoundException("Không tìm thấy người dùng"));
+        if (!Boolean.TRUE.equals(user.getIsActive())) {
+            throw new BadRequestException("Người dùng đã bị vô hiệu hóa");
+        }
+        user.setIsActive(false);
+    }
+
+    @Override
+    @Transactional
+    public void deleteUserByAdmin(UUID id) {
+        Users user = usersRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Không tìm thấy người dùng"));
+        if (Boolean.TRUE.equals(user.getIsActive())) {
+            throw new BadRequestException("Phải vô hiệu hóa người dùng trước khi xóa");
+        }
+        usersRepository.delete(user);
     }
 }
